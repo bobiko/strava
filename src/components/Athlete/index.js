8e02242f0d132bb7d18d714171a96eed598d495d
Jakub Dominik <jakub.dominik@1mm.eu>
2017-10-13 16:48:51 +0200

    small changes for athlete

--- src/components/Athlete/index.js
+++ src/components/Athlete/index.js
@@ -8,14 +8,18 @@ import {
 const Athlete = ({ result }) => (
     <Container style={{ marginTop: 100 + 'px' }}>
         <Row>
-            <Col lg={6} offset={3}>
+            <Col lg={6} offset={3} className="text-center offset-md-3">
                 <Card>
-                    <CardImg top src={result.profile} alt="Card image cap" width={180} style={{ width: 180 + 'px' }} />
+                    <div className="text-center">
+                    <CardImg top src={result.profile_medium} alt="Card image cap" width={180} style={{ width: 180 + 'px', borderRadius: 50+'%', padding: 10 }} className="text-center" />
+                    </div>
                     <CardBody>
                         <CardTitle>{result.firstname} {result.lastname}</CardTitle>
                         <CardSubtitle>{result.username}</CardSubtitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                        <Button>Button</Button>
+                        <CardText>
+                            { result.state } / { result.country }
+                        </CardText>
+                        
                     </CardBody>
                 </Card>
             </Col>
