83633aa6bdeefe9788108a234bb192b90bc8ae60
bobiko <me@bobiko.pl>
2017-10-27 23:28:18 +0200

    Refactoring - Activities -> AcitivitiesList

---
+++ src/components/ActivitiesListContainer/ActivitiesListContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class ActivitiesListContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
