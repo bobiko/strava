e168ff507f96fa106a69084c7ca6718b763e73bd
bobiko <me@bobiko.pl>
2017-11-02 22:17:48 +0100

    todo: dokonczyc pobieranie danych

--- src/components/ActivityContainer/ActivityContainer.js
+++ src/components/ActivityContainer/ActivityContainer.js
@@ -2,10 +2,13 @@ import React from "react";
 import Loader from "../LoaderContainer/LoaderContainer";
 import { Table, Container, Col, Row } from "reactstrap";
 
-import ActivitySingleItem from './ActivitySingleItem'
+import ActivitySingleItem from "./ActivitySingleItem";
+import { strava } from "../../config/";
 
 // import { strava } from "root/config";
 // const api = require('root/helpers/api');
+
+const api = require("../../helpers/api");
 
 class ActivityContainer extends React.Component {
   constructor(props) {
@@ -16,16 +19,33 @@ class ActivityContainer extends React.Component {
     };
   }
 
+  getAthleteData(url) {
+    api.fetchStrava(url).then(response => this.setAtheteData(response));
+  }
+
+  setAtheteData(result) {
+    console.log(result);
+    this.setState({
+      loading: false,
+      result: result
+    });
+  }
+  // todo: sprawdzic activity
   componentDidMount() {
+     let url = `${strava.path.base}${strava.activities}/${this.state.item.id}?${strava.path.token}${strava.token}`;
+     this.getAthleteData(url);
+
     this.setState({
       loading: false
-    })
+    });
   }
 
   render() {
-     const { loading, item } = this.state;
-           
-    return loading ? <Loader /> :<ActivitySingleItem item={item} />; 
+    const { loading, item } = this.state;
+
+    console.log(item);
+
+    return loading ? <Loader /> : <ActivitySingleItem activity={item} />;
   }
 }
 
--- src/components/ActivityContainer/ActivitySingleItem.js
+++ src/components/ActivityContainer/ActivitySingleItem.js
@@ -1,12 +1,23 @@
 import React from "react";
 // import { Link } from 'react-router-dom';
 import utils from "../../helpers/utils";
-import { Container, Col } from "reactstrap";
+import { Container, Col, Jumbotron, Button } from "reactstrap";
 
-const ActivitySingleItem = ({ item }) => (
+const ActivitySingleItem = ({ activity }) => (
   <Container style={{ marginTop: 100 }}>
     <Col>
-      <p>{item.name}</p>
+      <Jumbotron>
+        <h1 className="display-3">{activity.name}</h1>
+        <p className="lead"> {activity.description}</p>
+        <hr className="my-2" />
+        <p>
+          It uses utility classes for typgraphy and spacing to space content out
+          within the larger container.
+        </p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+      </Jumbotron>
     </Col>
   </Container>
 );
